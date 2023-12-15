6bf9b8d695f4d99806c92f74bc20d6a6ab0d2f63
mumiak-mitch <mumiaktech@gmail.com>
2023-12-15 21:10:21 +0300

    Viewing the business listings

--- listings/urls.py
+++ listings/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from . import views
+#from . import views
+from .views import HomeView, ListingDetailView
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    #path('', views.home, name='home'),
+    path('', HomeView.as_view(), name="home"),
+    path('listings/<int:pk>/', ListingDetailView.as_view(), name="listings"),
 ]
--- listings/views.py
+++ listings/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Listing
 
 # Create your views here.
-def home(request):
-    return render(request, 'home.html', {})
+#def home(request):
+#    return render(request, 'home.html', {})
+
+class HomeView(ListView):
+    model = Listing
+    template_name = 'home.html'
+
+class ListingDetailView(DetailView):
+    model = Listing
+    template_name = 'listing_details.html'
