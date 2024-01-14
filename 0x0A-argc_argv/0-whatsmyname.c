3a08529122d4bf942148113bdfd15e2f3d6c2621
mumiak-mitch <mumiaktech@gmail.com>
2024-01-13 16:21:46 -0800

    betty

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -9,12 +9,12 @@
  */
 int main(int argc, char *argv[])
 {
-    if (argc > 0)
-    {
-        printf("%s\n", argv[0]);
-        return 0;
-    }
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+		return (0);
+	}
 
-    return 1;
+	return (1);
 }
 
